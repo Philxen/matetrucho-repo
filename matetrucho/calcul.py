9cd7a7cbb60722a571023e1195474930a3e83b02
Felipe <90008820+Philxen@users.noreply.github.com>
2026-01-07 12:53:12 +0100

    Update calcul.py
    nouvelle fonction additionner

--- matetrucho/calcul.py
+++ matetrucho/calcul.py
@@ -6,6 +6,10 @@ class Calculateur:
         """Méthode 1 : Une addition simple"""
         return a + b
 
+    def additionner2(self, a, b):
+        """Méthode 1 : Une addition simple"""
+        return a + 5*b
+    
     def saluer(self):
         """Méthode 2 : Une interaction textuelle"""
         return f"Bonjour {self.nom}, le module matetrucho est prêt !"
